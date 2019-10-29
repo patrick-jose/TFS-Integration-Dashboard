00a282de7aecd53f1034e8c0afb859888a22ded3
Patrick José <patric-jose@hotmail.com>
2019-10-29 14:38:28 -0300

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Toro-Integration'`, () => {
+  it(`should have as title 'Integration'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Toro-Integration');
+    expect(app.title).toEqual('Integration');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Toro-Integration!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Integration!');
   });
 });
