6b538fd7ec59e53d4216e7fa6c2fda6749312612
Patrick José <patric-jose@hotmail.com>
2019-10-29 14:40:14 -0300

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Toro-Integration!');
+    expect(page.getTitleText()).toEqual('Welcome to Integration!');
   });
 
   afterEach(async () => {
